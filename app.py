af1bd9fd75d75791f148aa275ec791c4f5428aeb
nolan <nolanbconaway@gmail.com>
2017-08-09 22:03:57 -0400

    add get req

--- app.py
+++ app.py
@@ -54,6 +54,10 @@ def handler():
     # jsonify, return
     return flask.jsonify(**payload)
 
+@app.route('/', methods=['GET'])
+def mainpage():
+	return('Working...')
+
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
